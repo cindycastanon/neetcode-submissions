465015d42bf913089a28ec62d9c6af4dc79785f3
Cindy Castañon <cindycastanon@hotmail.com>
2026-07-31 16:09:47 -0400

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+
+        for(int n : nums){
+            if(s.contains(n)){
+                return true;
+            }
+
+            s.add(n);
+        }
+
+        return false;
+        
+    }
+}
